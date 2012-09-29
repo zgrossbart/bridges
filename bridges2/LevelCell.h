906402be6ad31b338b6b08e612c66f3b206966fb
Zack Grossbart <zack@grossbart.com>
2012-09-29 17:48:34 -0400

    Added the license to the top of the level cells and a few more code comments.

--- bridges2/LevelCell.h
+++ bridges2/LevelCell.h
@@ -1,10 +1,20 @@
-//
-//  LevelCell.h
-//  bridges2
-//
-//  Created by Zack Grossbart on 9/21/12.
-//
-//
+/*******************************************************************************
+ *
+ * Copyright 2012 Zack Grossbart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
 #import <UIKit/UIKit.h>
 
@@ -20,8 +30,19 @@
  */
 -(void)setBorderVisible:(bool) visible;
 
+/**
+ * The title label for cell in the table.
+ */
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+
+/**
+ * The main image for the call which shows a thumbnail of the level
+ */
 @property (strong, nonatomic) IBOutlet UIImageView *screenshot;
+
+/**
+ * The check mark which shows in the upper right corner once you've won a level.
+ */
 @property (strong, nonatomic) IBOutlet UIImageView *checkMark;
 
 @end
